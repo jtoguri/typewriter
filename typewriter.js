33dc3b1a4adc4624f7bd6c371afd42efdf9eb58e
Joshua Toguri <joshua.toguri@gmail.com>
2022-02-08 11:30:42 -0500

    added typewriter file

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+let counter = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, counter);
+  counter += 50;
+}
+setTimeout(() => process.stdout.write('\n'), counter);
